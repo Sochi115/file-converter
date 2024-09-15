5d1ed0484b83b4e48cbb5b1a142daed2fed38ff9
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2024-09-15 18:07:48 +0700

    Created base conversion app

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello World")
+import tkinter as tk
+import tkinter.filedialog
+from spire.pdf.common import *
+from spire.pdf import *
 
 
-if __name__ == "__main__":
-    main()
+class MainGUI:
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.geometry("400x400")
+        self.root.title("File Converter")
+
+        self.label = tk.Label(self.root, text="File Converter", font=("Arial", 18))
+        self.label.pack()
+
+        self.button = tk.Button(
+            self.root,
+            text="Select a file",
+            font=("Arial", 16),
+            command=self.get_filepath,
+        )
+        self.button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+        self.root.mainloop()
+
+    def get_filepath(self):
+        filename = tkinter.filedialog.askopenfilename()
+
+        doc_filename = filename.replace(".pdf", ".docx")
+
+        doc = PdfDocument()
+        doc.LoadFromFile(filename)
+        doc.SaveToFile(doc_filename, FileFormat.DOCX)
+        doc.Close()
+        tk.messagebox.showinfo(
+            title="Conversion Done",
+            message=f"File has been converted to {doc_filename}",
+        )
+
+
+MainGUI()
